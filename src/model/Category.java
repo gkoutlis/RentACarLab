58228ab634f39ee01007c931ad6555ae75cfc2a1
To_Onoma_Sou <koutlisgreg@gmail.com>
2026-05-08 10:12:44 +0300

    Add Category entity extending BaseEntity"

--- src/model/Category.java
+++ src/model/Category.java
@@ -1,4 +1,30 @@
 package model;
 
-public class Category {
+public class Category extends BaseEntity {
+        private String kind;
+
+        public Category(String kind){
+            super();
+            
+            this.kind = kind;
+        }
+
+        public Category(int id, String kind){
+            super(id);
+            this.kind = kind;
+        }
+
+        public String getKind(){
+        return kind;
+    }
+
+        public void  setKind(String kind){
+            this.kind = kind;
+        }
+
+    @Override
+        public String toString(){
+            return kind;
+        }
+
 }
